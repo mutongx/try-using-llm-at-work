8d41567ab0647eca724c02b6b18c9e946421c04b
Mut Ze <mutongsoft@gmail.com>
2023-07-29 20:54:43 +0900

    Add pull_subtrees.sh to track thirdparty dir

---
+++ pull_subtrees.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+git subtree pull --prefix thirdparty/capnproto https://github.com/capnproto/capnproto master --squash
+git subtree pull --prefix thirdparty/fmt https://github.com/fmtlib/fmt master --squash
+git subtree pull --prefix thirdparty/llama.cpp https://github.com/ggerganov/llama.cpp master --squash
