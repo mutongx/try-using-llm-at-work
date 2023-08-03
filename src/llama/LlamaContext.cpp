a7135d4b2ab8175ab5ea554355dd6f026bdb17b7
Mut Ze <mutongsoft@gmail.com>
2023-08-03 09:58:50 +0000

    Add model eval and predict implementation

--- src/llama/LlamaContext.cpp
+++ src/llama/LlamaContext.cpp
@@ -1,23 +1,97 @@
 #include "LlamaContext.h"
 
+#include <cstring>
+
 namespace muton::playground::llm {
 
 LlamaContext::LlamaContext(LlamaModel const& model, LlamaParams const& params)
-    : context_(llama_new_context_with_model(model, params)) {}
+    : context_(llama_new_context_with_model(model, params)), context_size_(params->n_ctx), tokens_(context_size_, 0) {}
 
 LlamaContext::LlamaContext(LlamaContext&& another) noexcept {
-  context_ = another.context_;
-  another.context_ = nullptr;
+  MoveFrom(std::move(another));
 }
 
 LlamaContext& LlamaContext::operator=(LlamaContext&& another) noexcept {
-  context_ = another.context_;
-  another.context_ = nullptr;
+  MoveFrom(std::move(another));
   return *this;
+}
+
+void LlamaContext::MoveFrom(LlamaContext&& another) noexcept {
+  context_ = another.context_;
+  context_size_ = another.context_size_;
+  tokens_ = std::move(another.tokens_);
+  tokens_begin_ = another.tokens_begin_;
+  tokens_size_ = another.tokens_size_;
+  tokens_eval_ = another.tokens_eval_;
+  another.context_ = nullptr;
 }
 
 LlamaContext::operator llama_context*() const {
   return context_;
+}
+
+bool LlamaContext::FeedBos() {
+  auto bos = llama_token_bos();
+  return Feed(std::span<llama_token>(&bos, 1));
+}
+
+bool LlamaContext::Feed(llama_token token_pending) {
+  return Feed(std::span<llama_token>(&token_pending, 1));
+}
+
+bool LlamaContext::Feed(std::span<llama_token> tokens_pending) {
+  if (tokens_size_ + tokens_pending.size() > context_size_) {
+    return false;
+  }
+  memcpy(tokens_.data() + tokens_size_, tokens_pending.data(), tokens_pending.size() * sizeof(llama_token));
+  tokens_size_ += tokens_pending.size();
+  return true;
+}
+
+bool LlamaContext::Eval(LlamaContext::EvalOption option) {
+  size_t to_eval = tokens_size_ - tokens_eval_;
+  while (to_eval > 0) {
+    size_t current_batch_size = std::min(to_eval, option.batch_size);
+    if (llama_eval(context_,
+                   tokens_.data() + tokens_eval_,
+                   static_cast<int>(current_batch_size),
+                   static_cast<int>(tokens_eval_),
+                   static_cast<int>(option.thread_count)) != 0) {
+      return false;
+    }
+    tokens_eval_ += current_batch_size;
+    to_eval -= current_batch_size;
+  }
+  return true;
+}
+
+llama_token LlamaContext::Predict(LlamaContext::PredictOption option) {
+  auto* logits = llama_get_logits(context_);
+  auto vocab_size = llama_n_vocab(context_);
+  std::vector<llama_token_data> candidates(vocab_size);
+  for (llama_token token_id = 0; token_id < vocab_size; ++token_id) {
+    candidates.emplace_back(llama_token_data{token_id, logits[token_id], 0.0F});
+  }
+  llama_token_data_array candidates_p = {candidates.data(), candidates.size(), false};
+
+  size_t repeat_penalty_size = std::min(std::min(tokens_size_, option.repeat_penalty_size), context_size_);
+  llama_sample_repetition_penalty(context_,
+                                  &candidates_p,
+                                  tokens_.data() + tokens_size_ - repeat_penalty_size,
+                                  repeat_penalty_size,
+                                  option.repeat_penalty);
+  llama_sample_frequency_and_presence_penalties(context_,
+                                                &candidates_p,
+                                                tokens_.data() + tokens_size_ - repeat_penalty_size,
+                                                repeat_penalty_size,
+                                                option.alpha_frequency,
+                                                option.alpha_presence);
+  llama_sample_top_k(context_, &candidates_p, option.top_k, 1);
+  llama_sample_tail_free(context_, &candidates_p, option.tail_free_z, 1);
+  llama_sample_typical(context_, &candidates_p, option.typical_p, 1);
+  llama_sample_top_p(context_, &candidates_p, option.top_p, 1);
+  llama_sample_temperature(context_, &candidates_p, option.temperature);
+  return llama_sample_token(context_, &candidates_p);
 }
 
 LlamaContext::~LlamaContext() {
--- src/llama/LlamaContext.h
+++ src/llama/LlamaContext.h
@@ -1,7 +1,10 @@
 #ifndef MUTON_PLAYGROUND_LLM_LLAMA_LLAMA_CONTEXT_H
 #define MUTON_PLAYGROUND_LLM_LLAMA_LLAMA_CONTEXT_H
 
+#include <span>
 #include <string>
+#include <thread>
+#include <vector>
 
 #include <llama.h>
 
@@ -21,8 +24,40 @@ class LlamaContext {
 
   operator llama_context*() const;
 
+  bool FeedBos();
+  bool Feed(llama_token token_pending);
+  bool Feed(std::span<llama_token> tokens_pending);
+
+  struct EvalOption {
+    size_t batch_size;
+    size_t thread_count;
+  };
+
+  bool Eval(EvalOption option);
+
+  struct PredictOption {
+    size_t repeat_penalty_size;
+    float repeat_penalty;
+    float alpha_presence;
+    float alpha_frequency;
+    int top_k;
+    float tail_free_z;
+    float typical_p;
+    float top_p;
+    float temperature;
+  };
+  llama_token Predict(PredictOption option);
+
  private:
-  llama_context* context_;
+  void MoveFrom(LlamaContext&& another) noexcept;
+
+  llama_context* context_{};
+  size_t context_size_{};
+
+  std::vector<llama_token> tokens_{};
+  size_t tokens_begin_{};
+  size_t tokens_size_{};
+  size_t tokens_eval_{};
 };
 
 }  // namespace muton::playground::llm
