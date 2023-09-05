966799a489d712e160b08c94d2a2c25dd950225c
Mut Ze <mutongsoft@gmail.com>
2023-09-05 05:40:59 +0000

    Add catch2 to subtrees, as we are going to write tests

--- pull_subtrees.sh
+++ pull_subtrees.sh
@@ -3,3 +3,4 @@
 git subtree pull --prefix thirdparty/capnproto https://github.com/capnproto/capnproto master --squash
 git subtree pull --prefix thirdparty/fmt https://github.com/fmtlib/fmt master --squash
 git subtree pull --prefix thirdparty/llama.cpp https://github.com/ggerganov/llama.cpp master --squash
+git subtree pull --prefix thirdparty/catch2 https://github.com/catchorg/Catch2 devel --squash
