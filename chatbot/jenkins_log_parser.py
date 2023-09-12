1b40f2661f8b4927768c098c8940093bfe0f9b2a
Mut Ze <mutongsoft@gmail.com>
2023-09-12 05:58:15 +0000

    Support parsing jenkins log as a tree

--- chatbot/jenkins_log_parser.py
+++ chatbot/jenkins_log_parser.py
@@ -1,5 +1,5 @@
 import re
-from typing import NamedTuple, List, Optional
+from typing import Any, NamedTuple, List, Optional, Generator
 
 
 class LogLine(NamedTuple):
@@ -10,22 +10,47 @@ class LogLine(NamedTuple):
 class LogItem(NamedTuple):
     title: Optional[str]
     lines: List[LogLine]
+    children: 'List[LogItem]'
 
 
 class JenkinsLogParser:
 
     def __init__(self):
-        self._items: List[LogItem] = [LogItem(title=None, lines=[])]
+        self._items: List[LogItem] = [LogItem(title=None, lines=[], children=[])]
+        self._finalized = False
 
     def feed(self, line: str):
         if line.startswith("[Pipeline] "):
-            self._items.append(LogItem(title=line, lines=[]))
+            self._items.append(LogItem(title=line.removeprefix("[Pipeline] "), lines=[], children=[]))
         else:
             match = re.match(r"^\[(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}.\d{3}Z)] (.*)", line)
             if match is not None:
-                self._items[-1].lines.append(LogLine(match.group(1), match.group(2)))
+                self._items[-1].lines.append(LogLine(timestamp=match.group(1), text=match.group(2)))
             else:
-                self._items[-1].lines.append(LogLine(None, line))
+                self._items[-1].lines.append(LogLine(timestamp=None, text=line))
 
     def finalize(self):
-        pass
+        if self._finalized:
+            return
+        stack = [self._items[0]]
+        first = True
+        for item in self._items:
+            if first:
+                first = False
+                continue
+            stack[-1].children.append(item)
+            if item.title.startswith("{"):
+                stack.append(item)
+            if item.title.startswith("}"):
+                stack.pop()
+        if len(stack) != 1:
+            raise RuntimeError("unable to parse pipeline tree")
+
+    @property
+    def items(self) -> Generator[LogItem, Any, Any]:
+        for item in self._items:
+            yield item
+
+    @property
+    def root(self) -> LogItem:
+        return self._items[0]
