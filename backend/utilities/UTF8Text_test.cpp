738aa3d5e715fe94a48b117d3498540b3e2aa94c
Mut Ze <mutongsoft@gmail.com>
2023-10-10 16:25:51 +0000

    update naming in UTF8Text

--- backend/utilities/UTF8Text_test.cpp
+++ backend/utilities/UTF8Text_test.cpp
@@ -2,13 +2,13 @@
 
 #include "utilities/UTF8Text.h"
 
-TEST_CASE("UTF8Text can parse unicode text", "[unicode]") {
-  using UnicodeText = muton::playground::llm::UTF8Text;
+TEST_CASE("UTF8Text can parse unicode text", "[utf8]") {
+  using UTF8Text = muton::playground::llm::UTF8Text;
   SECTION("Simple ASCII text") {
     std::vector<std::string> expected_str{"T", "e", "s", "t"};
     std::vector<uint32_t> expected_cp{'T', 'e', 's', 't'};
     size_t i = 0;
-    for (auto symbol : UnicodeText("Test")) {
+    for (auto symbol : UTF8Text("Test")) {
       REQUIRE(symbol.str == expected_str[i]);
       REQUIRE(symbol.cp == expected_cp[i]);
       ++i;
@@ -19,7 +19,7 @@ TEST_CASE("UTF8Text can parse unicode text", "[unicode]") {
     std::vector<std::string> expected_str{"你", "好"};
     std::vector<uint32_t> expected_cp{0x4F60, 0x597D};
     size_t i = 0;
-    for (auto symbol : UnicodeText("你好")) {
+    for (auto symbol : UTF8Text("你好")) {
       REQUIRE(symbol.str == expected_str[i]);
       REQUIRE(symbol.cp == expected_cp[i]);
       ++i;
@@ -30,7 +30,7 @@ TEST_CASE("UTF8Text can parse unicode text", "[unicode]") {
     std::vector<std::string> expected_str{"🥰", "😘", "🤣"};
     std::vector<uint32_t> expected_cp{0x1F970, 0x1F618, 0x1F923};
     size_t i = 0;
-    for (auto symbol : UnicodeText("🥰😘🤣")) {
+    for (auto symbol : UTF8Text("🥰😘🤣")) {
       REQUIRE(symbol.str == expected_str[i]);
       REQUIRE(symbol.cp == expected_cp[i]);
       ++i;
